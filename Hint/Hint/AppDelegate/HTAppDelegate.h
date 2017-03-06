cbdaf36f9b43dbc1f832c3d306973339b3836691
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-06 15:57:45 +0530

    Basic project structure.

---
+++ Hint/Hint/AppDelegate/HTAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/6/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Hint/Hint/AppDelegate/HTAppDelegate.m
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/6/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+
+}
+
+
+@end
