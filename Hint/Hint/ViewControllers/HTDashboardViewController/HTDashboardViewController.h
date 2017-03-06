33950d1e2210be290e0ccca6c9b02d430b4789c0
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-06 18:47:14 +0530

    Adding HTDashboardViewController

---
+++ Hint/Hint/ViewControllers/HTDashboardViewController/HTDashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTDashboardViewController.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/6/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTDashboardViewController : UIViewController
+
+@end
---
+++ Hint/Hint/ViewControllers/HTDashboardViewController/HTDashboardViewController.m
@@ -0,0 +1,26 @@
+//
+//  HTDashboardViewController.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/6/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "HTDashboardViewController.h"
+
+@interface HTDashboardViewController ()
+
+@end
+
+@implementation HTDashboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
