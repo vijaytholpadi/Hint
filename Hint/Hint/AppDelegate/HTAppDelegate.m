5d4f849f3f11fefe79b6ce4011f80e94d08b82c0
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-06 17:52:26 +0530

    Configure Parse integration.

--- Hint/Hint/AppDelegate/HTAppDelegate.m
+++ Hint/Hint/AppDelegate/HTAppDelegate.m
@@ -10,6 +10,7 @@
 
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
+#import <Parse/Parse.h>
 
 @interface HTAppDelegate ()
 
@@ -20,6 +21,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Fabric with:@[[Crashlytics class]]];
+    [self configureParse];
 
     return YES;
 }
@@ -49,5 +51,36 @@
 
 }
 
+- (void)configureParse {
+    [Parse initializeWithConfiguration:[ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration>  _Nonnull configuration) {
+        // Add your Parse applicationId:
+        configuration.applicationId = @"7e487db101990ea80db27b61463cbd9d4fed50ce";
+
+        // Uncomment and add your clientKey (it's not required if you are using Parse Server):
+         configuration.clientKey = @"db7f3e1b2b1c47be8cc739c5c0fb808f2f531fbd";
+
+        // Uncomment the following line and change to your Parse Server address;
+         configuration.server = @"http://10.47.184.96:80/parse";
+
+        // Enable storing and querying data from Local Datastore. Remove this line if you don't want to
+        // use Local Datastore features or want to use cachePolicy.
+        configuration.localDatastoreEnabled = YES;
+    }]];
+
+    // ****************************************************************************
+    // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
+    // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
+    // [PFFacebookUtils initializeFacebook];
+    // ****************************************************************************
+
+    [PFUser enableAutomaticUser];
+
+    PFACL *defaultACL = [PFACL ACL];
+
+    // If you would like all objects to be private by default, remove this line.
+    defaultACL.publicReadAccess = YES;
+
+    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+}
 
 @end
