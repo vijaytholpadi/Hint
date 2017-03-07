742ffcd7a430cbb8411515b61b98b837bad215db
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-07 12:24:27 +0530

    Adding UIColor+HTColor category.

---
+++ Hint/Hint/Categories/UIColor+HTColor/UIColor+HTColor.h
@@ -0,0 +1,14 @@
+//
+//  UIColor+HTColor.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HTColor)
++ (UIColor*)ht_BlueColor;
++ (UIColor*)ht_YellowColor;
+@end
---
+++ Hint/Hint/Categories/UIColor+HTColor/UIColor+HTColor.m
@@ -0,0 +1,19 @@
+//
+//  UIColor+HTColor.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "UIColor+HTColor.h"
+
+@implementation UIColor (HTColor)
++ (UIColor*)ht_BlueColor {
+    return [UIColor colorWithRed:(18/255.0) green:(64/255.0) blue:(171/255.0) alpha:1.0];
+}
+
++ (UIColor*)ht_YellowColor {
+    return [UIColor colorWithRed:(255/255.0) green:(221/255.0) blue:(57/255.0) alpha:1.0];
+}
+@end
