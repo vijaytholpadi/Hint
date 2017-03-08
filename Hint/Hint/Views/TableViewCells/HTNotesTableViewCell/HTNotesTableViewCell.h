65fac539b872fbba5410ad2b5a03875072db185c
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-08 15:12:25 +0530

    Adding HTNotesTableViewCell

---
+++ Hint/Hint/Views/TableViewCells/HTNotesTableViewCell/HTNotesTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  HTNotesTableViewCell.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//Frameworks
+#import <ParseUI/ParseUI.h>
+
+@interface HTNotesTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet PFImageView *noteImageView;
+@property (weak, nonatomic) IBOutlet UILabel *noteTextLabel;
+@end
---
+++ Hint/Hint/Views/TableViewCells/HTNotesTableViewCell/HTNotesTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  HTNotesTableViewCell.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "HTNotesTableViewCell.h"
+
+@implementation HTNotesTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
