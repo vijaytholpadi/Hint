423d1d5a8e949d53ca4f63d08063fcea77911405
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-08 14:50:38 +0530

    Adding HTBeacon

---
+++ Hint/Hint/Models/HTBeacon/HTBeacon.h
@@ -0,0 +1,20 @@
+//
+//  HTBeacon.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class CLBeacon;
+
+@interface HTBeacon : NSObject
+@property (nonatomic, strong) NSString *UUID;
+@property (nonatomic, strong) NSString *major;
+@property (nonatomic, strong) NSString *minor;
+@property (nonatomic, strong) NSArray *notes;
+
++ (instancetype)getHTBeaconFromCLBeacon:(CLBeacon*)beacon;
+@end
---
+++ Hint/Hint/Models/HTBeacon/HTBeacon.m
@@ -0,0 +1,24 @@
+//
+//  HTBeacon.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "HTBeacon.h"
+
+//Frameworks
+#import <CoreLocation/CoreLocation.h>
+
+@implementation HTBeacon
++ (instancetype)getHTBeaconFromCLBeacon:(CLBeacon*)beacon {
+    HTBeacon *htBeacon = [HTBeacon new];
+    htBeacon.UUID = beacon.proximityUUID.UUIDString;
+    htBeacon.major = beacon.major.stringValue;
+    htBeacon.minor = beacon.minor.stringValue;
+
+    return htBeacon;
+}
+
+@end
