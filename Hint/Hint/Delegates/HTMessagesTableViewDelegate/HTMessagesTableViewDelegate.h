b9e03d556a086d70a2cecf0e32626d920abf1ee2
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-08 15:05:14 +0530

    Adding HTMessagesTableViewDelegate

---
+++ Hint/Hint/Delegates/HTMessagesTableViewDelegate/HTMessagesTableViewDelegate.h
@@ -0,0 +1,14 @@
+//
+//  HTMessagesTableViewDelegate.h
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+//Frameworks
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface HTMessagesTableViewDelegate : NSObject<UITableViewDelegate>
+@end
---
+++ Hint/Hint/Delegates/HTMessagesTableViewDelegate/HTMessagesTableViewDelegate.m
@@ -0,0 +1,16 @@
+//
+//  HTMessagesTableViewDelegate.m
+//  Hint
+//
+//  Created by Vijay Tholpadi on 3/7/17.
+//  Copyright © 2017 TheGeekProjekt. All rights reserved.
+//
+
+#import "HTMessagesTableViewDelegate.h"
+
+@implementation HTMessagesTableViewDelegate
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    //TODO:Send event for Analytics Maybe?
+}
+
+@end
