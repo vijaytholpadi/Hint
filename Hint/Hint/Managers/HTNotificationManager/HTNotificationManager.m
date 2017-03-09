a70bddc5c56728a2b5c5f58d9b736277949a71a2
Vijay Tholpadi <vijay@thegeekprojekt.com>
2017-03-09 17:44:03 +0100

    Implementing BookKeeping and Regulation for Beacon Notifications.

--- Hint/Hint/Managers/HTNotificationManager/HTNotificationManager.m
+++ Hint/Hint/Managers/HTNotificationManager/HTNotificationManager.m
@@ -10,26 +10,60 @@
 
 //Categories
 #import "CLBeacon+NSCoding.h"
+#import "NSDate+HTAdditions.h"
 
 //Frameworks
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
+static NSString *kBeaconNotificationHistoryDictionaryKey = @"beaconNotificationHistoryDictionaryKey";
+static NSInteger kPollingCooldownPeriodInSecs = 120;
+
 @implementation HTNotificationManager
 
 + (void)fireNotificationForBeacon:(CLBeacon*)beacon {
-    if ([UIApplication sharedApplication].applicationState == UIApplicationStateBackground) {
-        UILocalNotification *notification = [[UILocalNotification alloc] init];
-        notification.alertBody = @"Psst! Check out this secret.";
-        notification.alertAction = @"View Note";
-        notification.soundName = @"default";
-        notification.userInfo = @{@"beacon":[beacon archivedData],
-                                  @"UUID":beacon.proximityUUID.UUIDString,
-                                  @"major":beacon.major,
-                                  @"minor":beacon.minor};
+    if ([NSDate getCurrentTimeStamp] - [HTNotificationManager getLastTriggeredTimeStampForBeaconMajor:beacon.major.stringValue andBeaconMinor:beacon.minor.stringValue] > kPollingCooldownPeriodInSecs) {
+        if ([UIApplication sharedApplication].applicationState == UIApplicationStateBackground) {
+            UILocalNotification *notification = [[UILocalNotification alloc] init];
+            notification.alertBody = @"Psst! Check out this secret.";
+            notification.alertAction = @"View Note";
+            notification.soundName = @"default";
+            notification.userInfo = @{@"beacon":[beacon archivedData],
+                                      @"UUID":beacon.proximityUUID.UUIDString,
+                                      @"major":beacon.major,
+                                      @"minor":beacon.minor};
 
-        [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
+            [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
+
+            [HTNotificationManager saveEntryForBeaconMajor:beacon.major.stringValue andBeaconMinor:beacon.minor.stringValue];
+        }
     }
+}
+
+
+#pragma mark - BookKeeping and Regulation
++ (void)saveEntryForBeaconMajor:(NSString*)beaconMajorString andBeaconMinor:(NSString*)beaconMinorString {
+    NSMutableDictionary *triggerHistory = [[[NSUserDefaults standardUserDefaults] objectForKey:kBeaconNotificationHistoryDictionaryKey] mutableCopy];
+
+    //Create record if never created.
+    if (!triggerHistory) {
+        triggerHistory = [NSMutableDictionary dictionary];
+    }
+
+    NSString *beaconCombinationKey = [NSString stringWithFormat:@"%@-%@", beaconMajorString, beaconMinorString];
+    triggerHistory[beaconCombinationKey] = [NSString stringWithFormat:@"%f", [NSDate getCurrentTimeStamp]];
+
+    [[NSUserDefaults standardUserDefaults] setObject:triggerHistory forKey:kBeaconNotificationHistoryDictionaryKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
++ (double)getLastTriggeredTimeStampForBeaconMajor:(NSString*)beaconMajorString andBeaconMinor:(NSString*)beaconMinorString {
+    NSMutableDictionary *triggerHistory = [[[NSUserDefaults standardUserDefaults] objectForKey:kBeaconNotificationHistoryDictionaryKey] mutableCopy];
+
+    NSString *beaconCombinationKey = [NSString stringWithFormat:@"%@-%@", beaconMajorString, beaconMinorString];
+
+    double lastTriggeredTimeStamp = [triggerHistory[beaconCombinationKey] doubleValue];
+    return lastTriggeredTimeStamp ? lastTriggeredTimeStamp : 0;
 }
 
 @end
